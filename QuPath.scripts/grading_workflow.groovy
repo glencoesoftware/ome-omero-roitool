ba6724d3c0a278c19fab2392b6224604f87c45d2
Melissa Linkert <melissa@glencoesoftware.com>
2020-02-13 20:50:21 -0600

    Add new script to make annotation grading easier

---
+++ QuPath.scripts/grading_workflow.groovy
@@ -0,0 +1,78 @@
+/**
+ * -----------------------------------------------------------------------------
+ *   Copyright (C) 2020 Glencoe Software, Inc. All rights reserved.
+ *
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * ------------------------------------------------------------------------------
+ *
+ * QuPath OME-XML Importer
+ *
+ * This script will import ROIs from an OME-XML file and generate detections and
+ * annotations for the current image opened in QuPath
+ *
+ * Instructions:
+ *   - Open the image to be annotated in QuPath
+ *   - Open this file in the QuPath "Script editor"
+ *   - Choose "Run" from the Run menu
+ *   - When prompted, choose the OME-XML import script
+ */
+
+import javafx.application.Platform
+import qupath.lib.gui.prefs.PathPrefs
+import qupath.lib.objects.classes.PathClassFactory
+
+gradingWorkflow()
+
+void gradingWorkflow() {
+    if (!Platform.isFxApplicationThread()) {
+        Platform.runLater({ gradingWorkflow() })
+        return
+    }
+
+    qupath = QPEx.getQuPath()
+
+    // prompt if annotation classes should be removed
+
+    def clearList = Dialogs.showYesNoDialog("Clear annotation list", "Clear annotation class list?")
+
+    if (clearList) {
+        def classList = qupath.getAvailablePathClasses()
+        classList.clear()
+    }
+
+    // perform OME-XML import
+    // automatically load the script if it is installed in the shared scripts directory
+    // otherwise, prompt for the script location
+
+    def pathProperty = PathPrefs.scriptsPathProperty()
+    def scriptName = "OME_XML_import.groovy"
+    if (pathProperty != null && pathProperty.getValue() != null) {
+        importScript = new File(pathProperty.getValue(), scriptName)
+    }
+    else {
+        importScript = qupath.getDialogHelper().promptForFile("Choose the OME_XML_import script", null, scriptName, ".groovy")
+    }
+    evaluate(importScript)
+
+    // add empty classes for grading
+
+    def classList = qupath.getAvailablePathClasses()
+    def gradeCount = 5
+    (0..(gradeCount - 1)).each { grade ->
+        def qpClass = PathClassFactory.getPathClass("Grade " + grade, null)
+        classList.add(qpClass)
+    }
+}
